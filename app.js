184d0b4c3a42f3a2fda69327a1ddc332225b4932
Franco Ezequiel Albornoz <franco.ezequiel@outlook.com>
2021-07-14 00:37:56 -0300

    agregado favicon y modificado gitignore

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.locals.moment = require('moment');
-
+app.use(favicon(path.join(__dirname,'public','favicon.png')));
 // routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
